0e354af53184544c07b2f3028dd7ae46bc46aa4b
Tom Pieper <tompieper@Toms-Mac-mini.local>
2021-04-18 19:46:44 +0200

    Second file if-Statement

--- second_file.py
+++ second_file.py
@@ -1,2 +1,17 @@
 print("Welcome to the second new created python file")
 print("I will be your host today")
+
+
+
+def addOrMultiply(bool, x, y):
+    if bool:
+        print("Multiplied:")
+        print(x*y)
+    else:
+        print("Added")
+        print(x+y)
+
+
+
+addOrMultiply(True, 10, 10)
+addOrMultiply(False, 10, 10)

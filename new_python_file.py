3f409aba68041187d36debf955d5cdab9a7e2fb4
Tom Pieper <tompieper@Toms-Mac-mini.local>
2021-04-18 19:17:59 +0200

    First new function

--- new_python_file.py
+++ new_python_file.py
@@ -4,3 +4,18 @@ print(9*8)
 
 print("ouh jeeeay")
 print("this totally fucking works")
+
+
+def newFunction(x,y):
+    print(x+y)
+    print(x*y)
+
+    for i in range(3,10):
+        if x % y == 0:
+            print("yes boi")
+        else:
+            print("nicht durch 3 teilbar")
+        x += i
+
+
+newFunction(18,3)
